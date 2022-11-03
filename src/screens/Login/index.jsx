e7aef86410c40646bf7709510daa804813feeaa5
juan <amarillojuanignacio8@gmail.com>
2022-11-03 09:28:20 -0300

    Clase login con react-hook-form

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,81 +1,32 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './login.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
+export default function login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-//HTML DE LA VENTANA LOGIN CON VALIDACIONES EN LOS INPUT Y BUTTON
-const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const onSubmit = data => {
+    console.log(data)
+  };
 
-  const [emailError, setEmailError] = useState(false);
-  const [passError, setPassError] = useState(false);
-
-  console.log("email", email);
-  console.log("pass", password);
-
-  /*const handleChangeEmail = (event) =>{
-    setEmail(event.target.value)
-  }
-
-  const handleChangePassword = (event) =>{
-    setPassword(event.target.value)
-  }*/
-
-  const handleChange = (event, name) =>{
-    switch (name) {
-      case "email":
-        setEmail(event.target.value);
-        break;
-      case "password":
-        setPassword(event.target.value);
-        break;
-      default:
-        break;
-    }
-  }
-
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    console.log("email", email);
-    console.log("password", password);
-
-    if (!email) return setEmailError(true);
-    if (!password) return setPassError(true);
-      console.log("Error en email");
-
-      setEmailError(false);
-      setPassError(false);
-  }
-
+  console.log(errors);
   return (
-    <div className={styles.container}>
-      <h2>Login</h2>
-      <form onSubmit={(handleSubmit)}>
+    <div>
+       <h2>Login</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Email</label>
+        <input type={"email"} {...register("email",{required: true})}/>
+        {errors.email && <span>This field is required</span>}
 
-      <label>Email</label>
-      <input type="email" value={email} onChange = {(event) => handleChange(event, "email")}/>
+        <label>Password</label>
+        <input type={"password"} {...register("password", { required: true, minLength: 6 })} />
+        {errors.password && <span>error password</span>}
 
-      {/* If ternarios (si el error está en true muestra un mensaje) */}
 
-      {
-        emailError && <p>Email incorrecto</p>
-      }
-
-      <label >Password</label>
-      <input type="password" value={password} onChange = {(event) => handleChange(event, "password")}/>
-
-      {
-        passError && <p>Password incorrecto</p>
-      }
-
-      <button type='submit'>Iniciar Sesión</button>
-
-      <Link to={'/'}>Ir al Welcome</Link>
-
+        <button type="submit">Login</button>
       </form>
+      <Link to={"/"}>Ir al welcome</Link> 
     </div>
-  );
-};
-
-export default Login;
+   
+  )
+}
